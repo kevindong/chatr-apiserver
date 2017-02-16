f04d2a0dc04a359bdc2b4eab4d95cdbce73d7aa4
meegul304 <micha3lbilstein@gmail.com>
2017-02-16 17:15:31 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('*', (req, res) => {
+	res.send('Hello from the api server!');
+});
+
+app.listen(port, () => {
+	console.log(`Api server listening on port ${port}`);
+});
