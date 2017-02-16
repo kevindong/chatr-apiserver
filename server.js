23404509af5503455361cdaa098d937544eb6a28
meegul304 <micha3lbilstein@gmail.com>
2017-02-16 17:16:05 -0500

    Added message that this is the dev server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('*', (req, res) => {
-	res.send('Hello from the api server!');
+	res.send('Hello from the dev api server!');
 });
 
 app.listen(port, () => {
